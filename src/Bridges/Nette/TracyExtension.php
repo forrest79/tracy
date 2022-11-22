61341ffcfe81fc051dcf17516b3828646b24de79
Jakub Trmota <jakub@trmota.cz>
2022-11-22 22:31:01 +0100

    Remote bars: add remote bars rendering, Chrome Dev panel

--- src/Bridges/Nette/TracyExtension.php
+++ src/Bridges/Nette/TracyExtension.php
@@ -62,6 +62,7 @@ class TracyExtension extends Nette\DI\CompilerExtension
 			'blueScreen' => Expect::listOf('callable'),
 			'editorMapping' => Expect::arrayOf('string')->dynamic()->default(null),
 			'netteMailer' => Expect::bool(true),
+			'remoteServerUrl' => Expect::string()->dynamic(),
 		]);
 	}
 
@@ -142,7 +143,8 @@ class TracyExtension extends Nette\DI\CompilerExtension
 			}
 
 			if (
-				!$this->cliMode
+				$options['remoteServerUrl'] === null
+				&& !$this->cliMode
 				&& Tracy\Debugger::getSessionStorage() instanceof Tracy\NativeSession
 				&& ($name = $builder->getByType(Nette\Http\Session::class))
 			) {
