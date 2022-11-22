deceeea5000e5e3e0c93de67448394c1bded80ce
Jakub Trmota <jakub@trmota.cz>
2022-11-22 22:31:01 +0100

    Remote bars: add remote bars rendering, Chrome Dev panel

--- src/Tracy/BlueScreen/BlueScreen.php
+++ src/Tracy/BlueScreen/BlueScreen.php
@@ -623,4 +623,15 @@ class BlueScreen
 
 		return [$generators, $fibers];
 	}
+
+
+	/********************* remote ****************f*79**/
+
+
+	public function remoteRender(\Throwable $exception): void
+	{
+		Debugger::remoteAdd(Helpers::capture(function () use ($exception) {
+			$this->renderTemplate($exception, __DIR__ . '/assets/page.phtml', false);
+		}));
+	}
 }
